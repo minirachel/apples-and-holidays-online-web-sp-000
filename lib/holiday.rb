4efab7be368108608d51e6711be9af7f9f2ac724
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-12-01 22:51:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -100,8 +100,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |seasons, days_hash|
-    days_hash.collect do |days, supplies|
-      supplies == "BBQ"
-    end
+    binding.pry
   end
 end
